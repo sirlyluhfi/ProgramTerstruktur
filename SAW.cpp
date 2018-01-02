850985851537b4948a59242d80c267a6bd2fd7b9
sirlyluthfi <32449911+sirlyluhfi@users.noreply.github.com>
2018-01-02 14:45:27 +0700

    Untuk Menampilkan Matrik X
    Menggunkan Looping For 2D

--- SAW.cpp
+++ SAW.cpp
@@ -5,4 +5,11 @@ using namespace std;
 int main(){
 	// DEKLARASI VARIABEL
      cout.setf(ios::fixed); cout.setf(ios::showpoint); cout.precision(4);
+     cout << "MATRIKS X : "<<endl;
+	double ac[3][5]={ {4,4,5,3,3},{3,3,4,2,3},{5,4,2,2,2} };
+	for(int i=0;i<3;i++){
+		for(int j=0;j<5;j++){
+		cout << ac[i][j] << " | ";
+		}cout<<endl;
+	}
 }
